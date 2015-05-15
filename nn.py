b4b9ebe1e0fc6091fe021c83af86de0e7609229d
Sten Sootla <stensoootla@gmail.com>
2015-05-15 18:50:54 +0300

    backpropagataion done

--- nn.py
+++ nn.py
@@ -18,28 +18,47 @@ class Network():
 
 	def train(self, training_data, training_labels): 
 		for i, instance in enumerate(training_data):
+			print i
 			# reshape array in order to easily calculate dot product
 			instance = instance.reshape(3072, 1)
 
-			result = self.feedforward(instance)
+			# feedforward
+			hidden_layer_input = np.dot(self.w1, instance) + self.bias1
 
-		probability_distribution = Network.softmax(result)
+			hidden_layer = Network.sigmoid(hidden_layer_input)
+			output_layer = np.dot(self.w2, hidden_layer) + self.bias2
 
-		# make prediction
-		# print "predicted class was: " + str(probability_distribution.argmax())
-		# print "real class was: " + str(training_labels[i].argmax())
+			# softmax probability distribution
+			probability_distribution = Network.softmax(output_layer)
 
-		return self.cross_entropy_loss(probability_distribution, training_labels[i])
+			# loss
+			loss = self.cross_entropy_loss(probability_distribution, training_labels[i])
 
-	def feedforward(self, a):
-		hidden_layer = Network.sigmoid(np.dot(self.w1, a) + self.bias1)
-		output_layer = np.dot(self.w2, hidden_layer) + self.bias2
+			# compute gradient for all weights from hidden layer to output layer
+			output_layer_gradient = probability_distribution - training_labels[i]
+			w2_gradient = np.dot(output_layer_gradient, hidden_layer.T)
 
-		return output_layer
+			self.w2 -= w2_gradient * 0.001
+
+			# compute gradient for all weights from input layer to hidden layer
+			dloss_dz = output_layer_gradient
+			dz_dy = self.w2
+
+			dy_da = Network.sigmoid_derivative(hidden_layer_input)
+
+			da_dw1 = instance
+
+			dloss_dz__dz_dy = np.dot(dloss_dz.T, dz_dy)
+			dloss_dz__dz_dy__dy_da = np.dot(dloss_dz__dz_dy.T, dy_da) 
+			w1_gradient = np.dot(dloss_dz__dz_dy__dy_da, da_dw1.T)  
+
+			self.w1 -= w1_gradient * 0.001
+
+		return loss
 
 	def cross_entropy_loss(self, probability_distribution, target_vector):
 		"""
-		Loss function.
+		Cross entropy loss function.
 		"""
 		return -np.sum((target_vector * np.log(probability_distribution)))
 
@@ -80,6 +99,10 @@ class Network():
 			an array of real values that are "squashed" into range between 0 and 1 by the function
 		""" 
 		return 1.0 / (1.0 + np.exp(-x))
+
+	@staticmethod
+	def sigmoid_derivative(x):
+		return np.dot((1-Network.sigmoid(x)).T, Network.sigmoid(x))
 
 root_path = '/home/sten/projects/neural_network/cifar_10_batches'
 d = load_cifar_batches(root_path)
