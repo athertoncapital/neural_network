6e3343df04451b785e7bd61444620ccbfe914521
Sten Sootla <stensoootla@gmail.com>
2015-05-18 20:47:06 +0300

    something is done

--- nn.py
+++ nn.py
@@ -1,60 +1,54 @@
-import math, pdb
+import math, bigfloat, pdb
 import numpy as np
 from data_utils import load_cifar_batches
 
 class Network():
+	count = 0
 
 	def __init__(self, sizes):
 		self.num_layers = len(sizes)
 		self.sizes = sizes
 
 		# initialize random weights
-		self.w1 = 0.001 * np.random.randn(sizes[1], sizes[0]) # hidden layer weights 
-		self.w2 = 0.001 * np.random.randn(sizes[2], sizes[1]) # output layer weights
+		self.w1 = 0.01 * np.random.randn(sizes[1], sizes[0]) # hidden layer weights 
+		self.w2 = 0.01 * np.random.randn(sizes[2], sizes[1]) # output layer weights
 
-		# initialize random biases
-		self.bias1 = np.random.randn(sizes[1], 1) # hidden layer biases 
-		self.bias2 = np.random.randn(sizes[2], 1) # output layer biases
+	def train(self, training_data, training_labels):
+		count = 0
 
-	def train(self, training_data, training_labels): 
 		for i, instance in enumerate(training_data):
-			print i
+			if not i % 100:
+				print i
+
+			# FEEDFORWARD
 			# reshape array in order to easily calculate dot product
-			instance = instance.reshape(3072, 1)
+			x = instance.reshape(3072, 1)
+			
+			y = Network.sigmoid(np.dot(self.w1, x))
+			z = np.dot(self.w2, y)
+			p = self.softmax(z)
+			
+			if p.argmax() == training_labels[i].argmax():
+				count += 1
 
-			# feedforward
-			hidden_layer_input = np.dot(self.w1, instance) + self.bias1
+			# loss = self.cross_entropy_loss(p, training_labels[i])
 
-			hidden_layer = Network.sigmoid(hidden_layer_input)
-			output_layer = np.dot(self.w2, hidden_layer) + self.bias2
+			# BACKPROPAGATION
+			# output layer (z) gradient
+			dloss_dz = p - training_labels[i]
+			dloss_dw2 = np.dot(dloss_dz, y.T)
 
-			# softmax probability distribution
-			probability_distribution = Network.softmax(output_layer)
+			# hidden layer (y) gradient
+			# dloss_dy = np.dot(self.w2.T, dloss_dz)
+			# dy_da = self.sigmoid_derivative(y)
+			# dy_dw1 = np.dot(dy_da, x.T)
+			# dloss_dw1 = np.dot(dloss_dy.T, dy_dw1)
 
-			# loss
-			loss = self.cross_entropy_loss(probability_distribution, training_labels[i])
+			# # perform parameter update
+			# self.w1 -= dloss_dw1
+			self.w2 -= dloss_dw2
 
-			# compute gradient for all weights from hidden layer to output layer
-			output_layer_gradient = probability_distribution - training_labels[i]
-			w2_gradient = np.dot(output_layer_gradient, hidden_layer.T)
-
-			self.w2 -= w2_gradient * 0.001
-
-			# compute gradient for all weights from input layer to hidden layer
-			dloss_dz = output_layer_gradient
-			dz_dy = self.w2
-
-			dy_da = Network.sigmoid_derivative(hidden_layer_input)
-
-			da_dw1 = instance
-
-			dloss_dz__dz_dy = np.dot(dloss_dz.T, dz_dy)
-			dloss_dz__dz_dy__dy_da = np.dot(dloss_dz__dz_dy.T, dy_da) 
-			w1_gradient = np.dot(dloss_dz__dz_dy__dy_da, da_dw1.T)  
-
-			self.w1 -= w1_gradient * 0.001
-
-		return loss
+		return float(count) / len(training_labels)
 
 	def cross_entropy_loss(self, probability_distribution, target_vector):
 		"""
@@ -62,8 +56,7 @@ class Network():
 		"""
 		return -np.sum((target_vector * np.log(probability_distribution)))
 
-	@staticmethod
-	def softmax(z):
+	def softmax(self, z):
 		"""
 		Takes a vector of arbitrary real-valued scores and squashes it to a vector of values 
 		between zero and one that sum to one. This vector represents a probability distribution
@@ -79,33 +72,46 @@ class Network():
 		numpy.ndarray
 			 a vector of values between zero and one that sum to one.
 		"""
-		return np.exp(z) / np.sum(np.exp(z))
+		f = z - np.max(z)
+		return np.exp(f) / np.sum(np.exp(f))
 
 	@staticmethod
 	@np.vectorize
 	def sigmoid(x):
-		""" 
-		A function that takes a real-valued number and "squashes" it into range between 0 and 1, so
-		that large negative numbers become 0 and large positive numbers become 1.  
+	    """ Numerically-stable sigmoid function. """
+	    if x >= 0:
+	        z = np.exp(-x)
+	        return 1 / (1 + z)
+	    else:
+	        # if x is less than zero then z will be small, denom can't be
+	        # zero because it's 1+z.
+	        z = np.exp(x)
+	        return z / (1 + z)
 
-		Parameters
-		----------
-		x : numpy.ndarray
-			an array of real values, each of them is given as an argument to the sigmoid function.
+	# @staticmethod
+	# def sigmoid(x):
+	# 	""" 
+	# 	A function that takes a real-valued number and "squashes" it into range between 0 and 1, so
+	# 	that large negative numbers become 0 and large positive numbers become 1.  
 
-		Returns
-		-------
-		numpy.ndarray
-			an array of real values that are "squashed" into range between 0 and 1 by the function
-		""" 
-		return 1.0 / (1.0 + np.exp(-x))
+	# 	Parameters
+	# 	----------
+	# 	x : numpy.ndarray
+	# 		an array of real values, each of them is given as an argument to the sigmoid function.
 
-	@staticmethod
-	def sigmoid_derivative(x):
-		return np.dot((1-Network.sigmoid(x)).T, Network.sigmoid(x))
+	# 	Returns
+	# 	-------
+	# 	numpy.ndarray
+	# 		an array of real values that are "squashed" into range between 0 and 1 by the function
+	# 	""" 
+	# 	return 1.0 / (1.0 + np.exp(-x))
+
+	def sigmoid_derivative(self, y):
+		return self.sigmoid(y) * (1 - self.sigmoid(y))
 
 root_path = '/home/sten/projects/neural_network/cifar_10_batches'
 d = load_cifar_batches(root_path)
 
 nn = Network([3072, 2000, 10])
 x = nn.train(d['training_data'], d['training_labels'])
+print "Accuracy: " + str(x)
